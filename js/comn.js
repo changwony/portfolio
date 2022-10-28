6f884b71f8e9223068cfb71cb40cb17aa2ac7dc5
changwony <asgod1203@naver.com>
2022-10-28 13:27:23 +0900

    3 commit

--- js/comn.js
+++ js/comn.js
@@ -2,7 +2,7 @@
 
 	var mouseCursor = document.querySelector('.cursor');
 	var projectLinks = document.querySelectorAll('.project-sec .p-box');
-	var basicLinks = document.querySelectorAll('.btn-pr-more');
+	var basicLinks = document.querySelectorAll('.btn-pr-more, .btn-contact');
 	var textZoom = document.querySelectorAll('.about-me p');
 	
 	window.addEventListener('scroll', cursor);
